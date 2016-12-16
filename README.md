be6822b2cf46a9a84207cad8d533e9903f4c32f7
Mattia de' Michieli Vitturi <demichie@gmail.com>
2016-12-16 16:17:47 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# IMEX_SfloW2D
+# template
+template program
+
+To compile:
+
+./configure
+make
+make install
+
+
+The executable is copied in the bin folder.
+
+To test the code copy the executable from the bin folder in the EXAMPLES folder and:
+
+./test
+
+A Matlab script (plot_solution.m) is provided in the MATLAB folder to plot the results.
+
+
---
+++ src/test.f90
@@ -0,0 +1,25 @@
+!********************************************************************************
+!> \mainpage   TEST  
+!> This is a template to create github projects.\n
+!>
+!> Version 0.1:\n
+!> 
+!> Github project page: http://demichie.github.io/template/
+!> \n
+!> \author Mattia de' Michieli Vitturi (*) \n
+!> (*) Istituto Nazionale di Geofisica e vulcanologia, sezione di Pisa\n
+!>     Via della Faggiola, 36\n
+!>     I-56126 Pisa, Italy \n
+!>     E-mail: mattia.demichielivitturi@ingv.it \n
+!*********************************************************************
+
+!> \brief Main Program 
+
+
+PROGRAM test
+  
+  IMPLICIT NONE
+ 
+  WRITE(*,*) 'Test program'
+
+END PROGRAM test
